8e1b5adbc0bcbaf00c99680e54b973a562314984
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-15 14:48:56 +0530

    Created a repository and a controller for when user click a contact , shows an error when the user doesnt exist in the app

--- lib/features/select_contact/controller/select_contact_controller.dart
+++ lib/features/select_contact/controller/select_contact_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/contact.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/features/select_contact/repository/select_contact_repository.dart';
 
@@ -5,3 +7,25 @@ final getContactsProvider = FutureProvider((ref) {
   final selectContactRep = ref.watch(selectContactRepositoryProvider);
   return selectContactRep.getContacts();
 });
+
+final selectContactControllerProvider = Provider((ref) {
+  final selectContactRepository = ref.watch(selectContactRepositoryProvider);
+  return SelectContactController(
+    ref: ref,
+    selectContactRrepository: selectContactRepository,
+  );
+});
+
+class SelectContactController {
+  final ProviderRef ref;
+  final SelectContactRrepository selectContactRrepository;
+
+  SelectContactController({
+    required this.ref,
+    required this.selectContactRrepository,
+  });
+
+  void selectContact(Contact selectedContact, BuildContext context) {
+    selectContactRrepository.selectContact(selectedContact, context);
+  }
+}

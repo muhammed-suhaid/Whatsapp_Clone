e805edbc12fcabc2ca94b939125bd991b35efefe
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-15 17:44:19 +0530

    created a chat_contact Model , message enums for messageType and chat repository

---
+++ lib/features/chat/repository/chat_repository.dart
@@ -0,0 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/enums/message_enum.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+import 'package:whatsapp_clone/models/chat_contact.dart';
+import 'package:whatsapp_clone/models/user_model.dart';
+
+class ChatRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+
+  ChatRepository({
+    required this.firestore,
+    required this.auth,
+  });
+
+  void _sendDataToContactsSubcollection(
+    UserModel senderUserData,
+    UserModel receiverUserData,
+    String text,
+    DateTime timeSent,
+    String receiverUserId,
+  ) async {
+    //----for sender
+    var receiverChatContact = ChatContact(
+      name: senderUserData.name,
+      profilePic: senderUserData.profilePic,
+      contactId: senderUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+    await firestore
+        .collection('users')
+        .doc(receiverUserId)
+        .collection('chats')
+        .doc(auth.currentUser!.uid)
+        .set(
+          receiverChatContact.toMap(),
+        );
+
+    //----for receiver
+    var senderChatContact = ChatContact(
+      name: receiverUserData.name,
+      profilePic: receiverUserData.profilePic,
+      contactId: receiverUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .collection('chats')
+        .doc(receiverUserId)
+        .set(
+          senderChatContact.toMap(),
+        );
+  }
+
+  void _saveMesssageToMessageSubcollection({
+    required String receiverUserId,
+    required String text,
+    required DateTime timeSent,
+    required String messageId,
+    required String userName,
+    required String receiverUserName,
+    required MessageEnum messageType,
+  }) async {}
+
+  void sendTextMessage({
+    required BuildContext context,
+    required String text,
+    required String receiverUserId,
+    required UserModel senderUser,
+  }) async {
+    try {
+      var timeSent = DateTime.now();
+      UserModel receiverUserData;
+
+      var userDataMap =
+          await firestore.collection('users').doc(receiverUserId).get();
+      receiverUserData = UserModel.fromMap(userDataMap.data()!);
+
+      _sendDataToContactsSubcollection(
+        senderUser,
+        receiverUserData,
+        text,
+        timeSent,
+        receiverUserId,
+      );
+
+      _saveMesssageToMessageSubcollection();
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}

5bf318417c8e7e718e10af17963bad3f98f79636
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-17 16:59:14 +0530

    dynamically displayed the offline and online status of user by didChangeAppLifecycleStatemethod provided on the WidgetsBindingObserver class

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -136,4 +136,11 @@ class AuthRepository {
           (event) => UserModel.fromMap(event.data()!),
         );
   }
+
+  void setUserState(bool isOnline) async {
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .update({'isOnline': isOnline});
+  }
 }

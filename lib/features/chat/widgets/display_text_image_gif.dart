3718e57aa48a96a46c04685a37b793f6323bc045
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-25 14:00:15 +0530

    Successfully sended the video and shown it in the chat

--- lib/features/chat/widgets/display_text_image_gif.dart
+++ lib/features/chat/widgets/display_text_image_gif.dart
@@ -1,10 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/enums/message_enum.dart';
+import 'package:whatsapp_clone/features/chat/widgets/video_player_item.dart';
 
 class DisaplayTextImageGif extends StatelessWidget {
   final String message;
   final MessageEnum type;
+
   const DisaplayTextImageGif({
     super.key,
     required this.message,
@@ -18,8 +20,8 @@ class DisaplayTextImageGif extends StatelessWidget {
             message,
             style: const TextStyle(fontSize: 16),
           )
-        : CachedNetworkImage(
-            imageUrl: message,
-          );
+        : type == MessageEnum.video
+            ? VideoPlayerItem(videoUrl: message)
+            : CachedNetworkImage(imageUrl: message);
   }
 }

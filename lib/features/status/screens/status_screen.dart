fa0e529e39ee33344644986736913663fb46fc4e
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-28 18:40:20 +0530

    got the status from the firebase

---
+++ lib/features/status/screens/status_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StatusScreen extends StatefulWidget {
+  const StatusScreen({super.key});
+
+  @override
+  State<StatusScreen> createState() {
+    return _StateStatusScreen();
+  }
+}
+
+class _StateStatusScreen extends State<StatusScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

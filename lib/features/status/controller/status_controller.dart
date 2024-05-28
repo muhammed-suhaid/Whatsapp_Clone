34bf355fedd1698fae88287547b1e4e7bf241de9
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-28 14:18:47 +0530

    created a model , controller  and repository for status

---
+++ lib/features/status/controller/status_controller.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
+import 'package:whatsapp_clone/features/status/repository/ststaus_repository.dart';
+
+final statusControllerProvider = Provider((ref) {
+  final statusRepository = ref.read(statusRepositoryProvider);
+  return StatusController(
+    statusRepository: statusRepository,
+    ref: ref,
+  );
+});
+
+class StatusController {
+  final StatusRepository statusRepository;
+  final ProviderRef ref;
+
+  StatusController({
+    required this.statusRepository,
+    required this.ref,
+  });
+
+  void addStatus(BuildContext context, File file) {
+    ref.watch(userDataAuthProvider).whenData((value) {
+      statusRepository.uploadStatus(
+        context: context,
+        username: value!.name,
+        profilePic: value.profilePic,
+        phoneNumber: value.phoneNumber,
+        statusImage: file,
+      );
+    });
+  }
+}
--- lib/features/status/repository/ststaus_repository.dart
+++ lib/features/status/repository/ststaus_repository.dart
@@ -1,6 +1,23 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whatsapp_clone/common/repository/common_firebase_storage_repository.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+import 'package:whatsapp_clone/models/status_model.dart';
+import 'package:whatsapp_clone/models/user_model.dart';
+
+final statusRepositoryProvider = Provider(
+  (ref) => StatusRepository(
+    firestore: FirebaseFirestore.instance,
+    auth: FirebaseAuth.instance,
+    ref: ref,
+  ),
+);
 
 class StatusRepository {
   final FirebaseFirestore firestore;
@@ -12,4 +29,87 @@ class StatusRepository {
     required this.auth,
     required this.ref,
   });
+
+  void uploadStatus({
+    required BuildContext context,
+    required String username,
+    required String profilePic,
+    required String phoneNumber,
+    required File statusImage,
+  }) async {
+    try {
+      var statusId = const Uuid().v1();
+      String uid = auth.currentUser!.uid;
+      String imageUrl = await ref
+          .read(commonFirebaseStorageRepositoryProvider)
+          .storeFileToFirebase(
+            '/status/$statusId$uid',
+            statusImage,
+          );
+      List<Contact> contacts = [];
+      if (await FlutterContacts.requestPermission()) {
+        contacts = await FlutterContacts.getContacts(withProperties: true);
+      }
+
+      List<String> uidWhoCanSee = [];
+
+      for (int i = 0; i < contacts.length; i++) {
+        var userDataFirebase = await firestore
+            .collection('users')
+            .where(
+              'phoneNumber',
+              isEqualTo: contacts[i].phones[0].number.replaceAll(' ', ''),
+            )
+            .get();
+
+        if (userDataFirebase.docs.isNotEmpty) {
+          var userData = UserModel.fromMap(userDataFirebase.docs[0].data());
+          uidWhoCanSee.add(userData.uid);
+        }
+      }
+
+      List<String> statusImageUrls = [];
+      var statusesSnapshot = await firestore
+          .collection('status')
+          .where('uid', isEqualTo: auth.currentUser!.uid)
+          .where(
+            'createdAt',
+            isLessThan: DateTime.now().subtract(
+              const Duration(hours: 24),
+            ),
+          )
+          .get();
+
+      if (statusesSnapshot.docs.isNotEmpty) {
+        Status status = Status.fromMap(
+          statusesSnapshot.docs[0].data(),
+        );
+        statusImageUrls = status.photoUrl;
+        statusImageUrls.add(imageUrl);
+
+        await firestore
+            .collection('status')
+            .doc(statusesSnapshot.docs[0].id)
+            .update({'photoUrl': statusImageUrls});
+        return;
+      } else {
+        statusImageUrls = [imageUrl];
+      }
+
+      Status status = Status(
+        uid: uid,
+        username: username,
+        phoneNumber: phoneNumber,
+        photoUrl: statusImageUrls,
+        createdAt: DateTime.now(),
+        profilePic: profilePic,
+        statusId: statusId,
+        whoCanSee: uidWhoCanSee,
+      );
+
+      await firestore.collection('status').doc(statusId).set(status.toMap());
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
 }

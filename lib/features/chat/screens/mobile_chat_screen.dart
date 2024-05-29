64ab1174afd6dde7502686291859402ca6c645e0
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-29 18:19:20 +0530

    Call data is stored into the firebase

--- lib/features/chat/screens/mobile_chat_screen.dart
+++ lib/features/chat/screens/mobile_chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/common/widgets/loader.dart';
 import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
+import 'package:whatsapp_clone/features/call/controller/call_controller.dart';
 import 'package:whatsapp_clone/features/chat/widgets/bottom_chat_field.dart';
 import 'package:whatsapp_clone/features/chat/widgets/chat_list.dart';
 import 'package:whatsapp_clone/models/user_model.dart';
@@ -13,12 +14,24 @@ class MobileChatScreen extends ConsumerWidget {
   final String name;
   final String uid;
   final bool isGroupChat;
+  final String profilePic;
   const MobileChatScreen({
     super.key,
     required this.name,
     required this.uid,
     required this.isGroupChat,
+    required this.profilePic,
   });
+
+  void makeCall(WidgetRef ref, BuildContext context) {
+    ref.read(callControllerProvider).makeCall(
+          context,
+          name,
+          uid,
+          profilePic,
+          isGroupChat,
+        );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -51,7 +64,7 @@ class MobileChatScreen extends ConsumerWidget {
         centerTitle: false,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => makeCall(ref, context),
             icon: const Icon(Icons.video_call),
           ),
           IconButton(
